c7f0db04830821faf86f71e1ea1b017bb1b0e9ec
finnluii <22249550+finnluii@users.noreply.github.com>
2020-10-01 13:52:48 -0400

    Thumbnails can how be moved to the side vertically! still some problem with highlighted thumbnails

--- content.js
+++ content.js
@@ -4,9 +4,35 @@
 // 3. Add the saved thumbnail element to the <side> bar
 let thumbnailsDiv = document.getElementsByClassName("gSTPzb")[0];
 
+let thumbnailChildren = document.getElementsByClassName("ZEZETe");
+
 let topDivClass = "GhN39b";
 let bottomDivClass = "G1OBde";
 let middleDivClass = "p2hjYe rCuEk";
+
+//const originalCSS = 
+
+function verticalizeThumbnails(thumbnails) {
+	// Get all child elements, and rearrange vertically
+	// var childNodes = thumbnails.childNodes; 
+
+	// console.log(childNodes);
+	console.log(thumbnailChildren);
+	thumbnailsDiv.style.overflow = "visible";
+	for (var i = 0; i < thumbnailChildren.length; i++) {
+		thumbnailChildren[i].style.display = "block";
+		thumbnailChildren[i].style.margin = "auto";
+	}
+}
+
+function horizontalizeThumbnails(thumbnails){
+	// var childNodes = thumbnails.childNodes;
+	thumbnailsDiv.style.overflow = "auto";
+	for (var i = 0; i < thumbnailChildren.length; i++) {
+		thumbnailChildren[i].style.display = "inline-block";
+		//thumbnailChildren[i].style.textAlign = "center";
+	}
+}
 
 chrome.runtime.onMessage.addListener(function(request) {
     if(request.action === 'changeToTop') {
@@ -18,6 +44,7 @@ chrome.runtime.onMessage.addListener(function(request) {
 		parentNode = thumbnailsDiv.parentNode.removeChild(thumbnailsDiv);
 		// Add the thumbnails to the top div 
 		topBar.appendChild(thumbnailsDiv);
+		horizontalizeThumbnails(thumbnailsDiv);
 
     } else if(request.action === 'changeToBottom') {
 		let bottomBar = document.getElementsByClassName(bottomDivClass)[0];
@@ -25,6 +52,7 @@ chrome.runtime.onMessage.addListener(function(request) {
 		parentNode = thumbnailsDiv.parentNode.removeChild(thumbnailsDiv);
 		// Add the thumbnails to the bottom div 
 		bottomBar.appendChild(thumbnailsDiv);
+		horizontalizeThumbnails(thumbnailsDiv);
 
     } else if (request.action === 'changeToLeft') {
     	// Loop through children thumbnail nodes
@@ -38,7 +66,11 @@ chrome.runtime.onMessage.addListener(function(request) {
     	After that, for each thumbnail, add it to a new line so it is vertically stacked
     	instead of horizontal.
     	*/
-    	videoBar.appendChild(thumbnailsDiv);
+    	let topBar = document.getElementsByClassName(topDivClass)[0];
+    	topBar.appendChild(thumbnailsDiv);
+    	verticalizeThumbnails(thumbnailsDiv);
+    	
+
 
     } else if (request.action === 'hideThumbnails') {
     	thumbnailsDiv.style.display = "none";
